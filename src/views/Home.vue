a602e3d69c33e042af24a5a8a2a5f40768a3b40d
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-12-08 12:56:36 -0800

    Fix typo in phone number.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
         <p class="text-gray-700 text-base mr-1">
           Call
         </p>
-        <a class="text-blue-400 mr-1" href="tel:+17604049532">760-404-7532</a>
+        <a class="text-blue-400 mr-1" href="tel:+17604047532">760-404-7532</a>
         <p class="text-gray-700 text-base mr-1">
           or email
         </p>
