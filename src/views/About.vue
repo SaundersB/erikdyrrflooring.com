aaac15d8f7729b892abf16021d7ea5dae6f5fce1
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-12-08 12:38:02 -0800

    Solve the break in the About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="items-center lg:h-auto m-auto p-10">
-    <div class="max-w-sm w-full justify-center lg:max-w-full lg:flex">
+  <div class=" lg:h-auto m-auto p-10">
+    <div class="w-full justify-center lg:max-w-full lg:flex md:flex flex flex-wrap">
       <img
-        class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+        class="h-48 lg:h-auto  lg:w-48 md:w-48 flex-none bg-cover rounded-t-none
+        rounded-l text-center overflow-hidden"
         src="../assets/img/tile_seven.png"
-        alt="Avatar of Jonathan Reinink"
+        alt="Erik Dyrr laying tile for over 20 years"
       />
       <div
         class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b
