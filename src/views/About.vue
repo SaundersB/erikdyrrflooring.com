12b56ff9646fa999bc4f18ab1cedff1a0edd9085
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-12-07 21:03:34 -0800

    Update the about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="about">
-    <h1>
-      We've been serving the High Desert region for over 20 years. From carpet,
-      laminate, to tile we handle it all!
-    </h1>
-  </div>
+    <div class="items-center lg:h-auto m-auto p-10">
+    <div class="max-w-sm w-full justify-center lg:max-w-full lg:flex">
+      <img class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" src="../assets/img/tile_seven.png" alt="Avatar of Jonathan Reinink">
+      <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div class="mb-8">
+          <div class="text-gray-900 font-bold text-xl mb-2">Over 20 years of service</div>
+          <p class="text-gray-700 text-base">Since 1972, Erik Dyrr has been providing flooring services in the High Desert region. From carpet,
+            laminate, to tile we handle it all!</p>
+        </div>
+      </div>
+    </div>
+    </div>
 </template>
