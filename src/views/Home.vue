33b997b7bc8482bd72547447c460bc7be88e2e50
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-12-08 16:10:22 -0800

    Hide the email address from the plain text.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,9 +16,9 @@
         </p>
         <a class="text-blue-400 mr-1" href="tel:+17604047532">760-404-7532</a>
         <p class="text-gray-700 text-base mr-1">
-          or email
+          or
         </p>
-        <a class="text-blue-400 mr-1" href="mailto:erikdyrr@live.com">erikdyrr@live.com</a>
+        <a class="text-blue-400 mr-1" href="mailto:erikdyrr@live.com">email us!</a>
       </div>
     </div>
     <carousel
